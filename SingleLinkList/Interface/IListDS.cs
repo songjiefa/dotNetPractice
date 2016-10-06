98c2f33d53dd336fc7692518e3b0af1afbd89076
Jeff Song <Jeff Song@JSONGSURFACE>
2016-10-07 00:51:17 +0800

    add Link list class

---
+++ SingleLinkList/Interface/IListDS.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkList.Interface
+{
+	interface IListDS<T>
+	{
+		int GetLength();
+		bool IsEmpty();
+		void Clear();
+		void Append(T i_Item);
+		void Insert(T i_Item, int i_Index);
+		T Delete(int i_Index);
+		T GetElement(int i_Index);
+		int Locate(T i_Value);
+	}
+}
---
+++ SingleLinkList/LinkList.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkList
+{
+	class LinkList<T> : Interface.IListDS<T>
+	{
+		private Node<T> head;
+
+		internal Node<T> Head
+		{
+			get
+			{
+				return head;
+			}
+
+			set
+			{
+				head = value;
+			}
+		}
+
+		public LinkList()
+		{
+			head = null;
+		}
+
+		void Interface.IListDS<T>.Append(T i_item)
+		{
+			var appendItem = new Node<T>(i_item);
+			var position = new Node<T>();
+
+			if(head == null)
+			{
+				head = appendItem;
+				return;
+			}
+
+			position = head;
+
+			while(position.Next != null)
+			{
+				position = position.Next;
+			}
+
+			position.Next = appendItem;
+		}
+
+		void Interface.IListDS<T>.Clear()
+		{
+			head = null;
+		}
+
+		T Interface.IListDS<T>.Delete(int i_Index)
+		{
+			if(this.InternalIsEmpty() || i_Index < 0)
+			{
+				Console.WriteLine("Link is empty or position is error!");
+				return default(T);
+			}
+
+			var deleteItem = new Node<T>();
+
+			if (i_Index == 1)
+			{
+				deleteItem = head;
+				head = head.Next;
+				return deleteItem.Data;
+			}
+
+			int tempIndex = 1;
+			var position = head;
+			var preDeleteItem = new Node<T>();
+
+			while(tempIndex < i_Index && position.Next != null)
+			{				
+				++tempIndex;
+				preDeleteItem = position;
+				position = position.Next;
+			}
+
+			if(tempIndex == i_Index)
+			{
+				preDeleteItem.Next = position.Next;
+				return position.Data;
+			}
+			else
+			{
+				Console.WriteLine("The ithem node is not exist!");
+				return default(T);
+			}
+		}
+
+		T Interface.IListDS<T>.GetElement(int i_Index)
+		{
+			if (this.InternalIsEmpty() || i_Index < 0)
+			{
+				Console.WriteLine("Link is empty or position is error!");
+				return default(T);
+			}
+
+			int tempIndex = 1;
+			var position = head;
+			var preGetItem = new Node<T>();
+
+			while (tempIndex < i_Index && position.Next != null)
+			{
+				++tempIndex;
+				preGetItem = position;
+				position = position.Next;
+			}
+
+			if (tempIndex == i_Index)
+			{
+				return position.Data;
+			}
+			else
+			{
+				Console.WriteLine("The ithem node is not exist!");
+				return default(T);
+			}
+		}
+
+		int Interface.IListDS<T>.GetLength()
+		{
+			return InternalGetLength();
+		}
+
+		void Interface.IListDS<T>.Insert(T i_Item,int i_Index)
+		{
+			if(i_Index < 1 || this.InternalIsEmpty())
+			{
+				Console.WriteLine("List is empty or position is error!");
+			}
+
+			var insertItem = new Node<T>(i_Item);
+			var preItem = new Node<T>();
+			var position = head;
+			var tempIndex = 1;
+
+			if(i_Index == 1)
+			{
+				insertItem.Next = position.Next;
+				position.Next = insertItem;
+				return;
+			}
+
+			while(tempIndex < i_Index && position.Next != null)
+			{
+				preItem = position;
+				position = position.Next;
+				++tempIndex;
+			}
+
+			if(tempIndex == i_Index)
+			{
+				preItem.Next = insertItem;
+				insertItem.Next = position;
+			}
+			else
+			{
+				Console.WriteLine("position error!");
+				return;
+			}
+		}
+
+		bool Interface.IListDS<T>.IsEmpty()
+		{
+			return InternalIsEmpty();
+		}
+
+		int Interface.IListDS<T>.Locate(T value)
+		{
+			var position = head;
+			var tempIndex = 1;
+
+			if (this.InternalIsEmpty())
+			{
+				Console.WriteLine("List is Empty!");
+				return -1;
+			}
+
+			while(!position.Data.Equals(value) && position.Next != null)
+			{
+				position = position.Next;
+				++tempIndex;
+			}
+
+			return tempIndex;
+		}
+
+		private int InternalGetLength()
+		{
+			var position = head;
+
+			int length = 0;
+			while (position != null)
+			{
+				++length;
+				position = position.Next;
+			}
+
+			return length;
+		}
+
+		private bool InternalIsEmpty()
+		{
+			if (head == null || head.Next == null)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
