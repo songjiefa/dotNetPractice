c26d31785ac39f111507a9a0d94c4f3a4d2f8a4c
Jeff Song <Jeff Song@JSONGSURFACE>
2016-10-08 18:44:02 +0800

    Create link list

--- SingleLinkList/Program.cs
+++ SingleLinkList/Program.cs
@@ -10,6 +10,24 @@ namespace SingleLinkList
 	{
 		static void Main(string[] args)
 		{
+			var link = CreateLinkList();
+		}
+
+		static LinkList<String> CreateLinkList()
+		{
+			var result = new LinkList<String>();
+			var input = Console.ReadLine();
+
+			result.Head = new Node<String>();
+			var position = result.Head;
+
+			while(input != "qq")
+			{
+				position.Next = new Node<String>(input);
+				position = position.Next;
+				input = Console.ReadLine();
+			}
+			return result;
 		}
 	}
 }
