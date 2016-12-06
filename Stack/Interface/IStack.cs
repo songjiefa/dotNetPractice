27878ed8d3065b231800d26a0ea557bd53e79391
Jeff Song <Jeff Song@JSONGSURFACE>
2016-12-06 14:32:31 +0800

    Stack interface

---
+++ Stack/Interface/IStack.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack.Interface
+{
+	public interface IStack<T>
+	{
+		int GetLength();
+		bool IsEmpty();
+		void Clear();
+		void Push(T i_item);
+		T Pop();
+	}
+}
