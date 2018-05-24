1b2fb0533997154c50393574c560e2b4a41ce708
songjiefa <714737954@qq.com>
2018-05-24 10:34:25 +0800

    TEST

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿SELECT SUM(Hours) FROM dbo.[Table] WHERE Name = N'张军' AND	 Date >= '05/01/2007' AND	Date <='05/31/2007'
+
+SELECT Name,SUM(Hours) FROM dbo.[Table] GROUP BY Name
+
+SELECT Name,SUM(Hours) FROM dbo.[Table] WHERE Date > '04/30/2007'GROUP BY Name HAVING SUM(Hours) > 16 
