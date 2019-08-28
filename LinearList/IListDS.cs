03ca902eeab97a99b85b2d824e9e81c2bce19915
songjiefa <714737954@qq.com>
2019-08-28 17:05:08 +0800

    Add custom list interface

---
+++ LinearList/IListDS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearList
+{
+    public interface IListDS<T>
+    {
+        int GetLength();
+        void Add(T i_item);
+        T Remove(int i_index);
+        T Insert(T i_item, int i_index);
+        T GetItem(int i_index);
+
+        void Clear();
+        bool isEmpty();
+        int Location(T i_item);
+    }
+}
---
+++ LinearList/SequenceList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearList
+{
+    class SequenceList<T> : IListDS<T>
+    {
+        private T[] m_list = new T[4];
+        public void Add(T i_item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetItem(int i_index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetLength()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Insert(T i_item, int i_index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool isEmpty()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Location(T i_item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Remove(int i_index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
